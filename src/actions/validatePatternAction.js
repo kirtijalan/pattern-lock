aace3bb54bb89b64ac30c913f9f83456e0d417f1
kirtijalan <kirti.jalan@ril.com>
2018-07-02 01:28:24 +0530

    code authentication

--- src/actions/validatePatternAction.js
+++ src/actions/validatePatternAction.js
@@ -1,1 +1,26 @@
 import * as types from './actionTypes';
+import { createCookie, readCookie, eraseCookie } from './../utils/cookie';
+
+function validatePatternResponse(info){
+    return {
+      type: types.VALIDATE_PATTERN,
+      codeSet: info.codeSet
+    }
+  }
+  
+
+export function validatePattern(data, callback) {
+    let setCodeVal = readCookie("code");
+    return function (dispatch) {
+        if(data === setCodeVal){   
+            createCookie("A", true, 730); 
+            dispatch(validatePatternResponse({success: true}));
+            callback();
+        }
+        else{
+            eraseCookie("A");
+            dispatch(validatePatternResponse({success: false}));
+            callback(); 
+        }  
+    }
+}
